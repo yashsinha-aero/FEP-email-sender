cbbbe6d05942cbec85e3d6676452f8d286f6fb3d
Yash Sinha <yash.sinha2006@gmail.com>
2026-06-24 02:59:16 +0530

    Port dynamic

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV !== "production") {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json());
 
